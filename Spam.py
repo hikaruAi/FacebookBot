36b14778ea68ffb10562946808c46adcd3eb89ab
hikaruAi <contacto.juanmendoza@gmail.com>
2016-01-31 15:08:41 -0600

    More spam

--- Spam.py
+++ Spam.py
@@ -27,3 +27,143 @@ spam.append("""
 | /
 Lﾉ﻿""")
 
+spam.append("""
+Estoy cansado de que mi mama me haga elegir entre ir a Dubai o ir a Disney. Osea, vieja, no quiero nada, tengo que estudiar para sacar biologia y entrar Harvard. Después de que discutimos me fui a mi mansion y me encerré en la suit a llorar. Y con qué vino para calmarme? Con el iPhone 7. lgual ni lo acepté. Se lo tire por la ventana. Sali corriendo hasta el garage porque me queria ir a la mierda. Pero no sabia que auto elegir, entoces elegi el ferrari rojo y me fui hasta el aeropuerto asi me voy a Hollywood con mi tia
+
+Como odio mi vida
+Ojala nunca les pase""")
+
+spam.append("""
+No se que intentas lograr pequeño pendejo, por idiotas como tu el mundo esta como esta, pinche negro de la selva noroestica, puedes irte a escribir pendejadas a otro lado pero seguramente te quemarian vivo por todo el cancer visual que causarias con tus posts muy machitos y cumbieros, pero puedes irte a la verga grande con todas tus estupideces echas realidad que posteas en grupos pendejos de facebook como buen buitre virgo sin vida social ni diversion.
+Por pendejos como tu el mundo esta como esta y no legalizan a las de 16.
+-10 papu, denunciado y 0 fav, buen intento pequeño engendro de Satanas y espero ha yas aprendido la leccion.
+Suerte para la proxima lince del infierno americano.""")
+
+spam.append("""
+Hola pinche puta sabrosa, he estado viendo tus fotos y se me para y tambien se pone bien dura mi verga. Con ganas de apretarte las tetas y poner mi verga entre ellas, con ese culote que tienes me dan ganas de meter mi verga por tu chumuelo. Pinche puta estas bien sabrosa quiero aventar mis mecos en tus sheshotas y en tu cara de puta adicta al sexo. Adeoz pinche puta rica""")
+
+spam.append("""
+Hijo de tu puta perra madre, que no te han enseñado que a las mujeres no se les pega? Seguramente no, porque tu papá es un pinche alcohólico de mierda, que sólo embarazó a tu puta madre y la dejo con un enjendro adrentro a la zorrita. Te gustaría que le hicieran lo mismo a la perra de tu mamá? Pues ni modo, porque ahora siempre que vaya al prostíbulo donde trabaja le daré unos putazos, hasta que se arrepienta de tener una escoria de hijo como tú. Llevaré a unos negros para que la violen y la dejen toda puteada para que cuando llegue a tu casa se ponga a llorar y a lamentarse de no haberte abortado, pinche putito.""")
+
+spam.append("""
+Necesito su ayuda. Se supone que para mi cumpleaños mi hermana mayor me ha dicho que me regalará un animal, y me ha dado a elegir entre una chinchilla y un hurón, sin embargo no sé cuál escoger, porque ya he tenido ambos y me han encantado. El punto es que la pendeja esa sabe que cuando me cuesta elegir entre dos cosas que me gustan, me frustro y termino sin tener ninguna para evitar la decisión, pero no me va a hacer lo mismo por otro año más. ¿Cuál sería mejor?""")
+
+spam.append("""
+Donde quedó La Perra Sin Corazón? JAJAJAJAJA Enculada, Enculada, Enculada y Despues decís que No xD 2016 Vas Sorprendiendo""")
+
+spam.append("""
+The UFOs 💫 have finally cum send this to 10 alien daddys if you get none back 🙅 you a lonely galactic hoe get five back you a Super Star Slut💫💥 get 10 backyou a super space freak 💫 happy fucking 💯 get that alien cock """)
+
+spam.append("""
+bubble gum 😋🍬 bubble gum in 🏻 a dish 🍽🏼 how many 🤔 times 🕐🕟🕓 can i succ that dicc send this to 🔟 of your 🏼sweetest 🍭 hoes""")
+
+spam.append("""
+Liked, subscribed, favorited. Shared on Twitter, Google+, Facebook, Instagram, Pinterest, and Tumblr. I burned the video to DVD/Blu Ray/thumb drives, printed a code people can scan on it with their cell phones, which automatically tags the video on their Facebook, Twitter, Tumblr, Google+, Instagram, and Pinterest, and it subscribes them to your channel, emails you a message of support, and likes and favorites the video. I sold hundreds of copies of the DVD/Blu Ray/thumb drives of this video, and all proceeds will go to a dog shelter, who will receive tickets to a raffle the shelter will give away to a live stream-a-thon of your videos. And I downloaded this video, printed out every frame, and had each one tattooed on one square 1/18" on my back""")
+
+spam.append("""
+I want to say something before this whole thing gets out of control. In 1967, in the hot desert sun, a woman gave birth to a boy in the back of old pick up. She was a whore. She left the boy at a local church. The father at the church didn't like children, but he knew that being a disciple of the bible he couldn't possibly not take the small kid. Instead he ended up taking out his frustration on the boy. At the age of three, the boy was forced to use the toilet. Let me tell you the little fell into his own pile of poop more than once. By the age of 5, the boy the boy was reading the bible and chopping wood for the fire. Despite the father's harsh character, the boy loved him and thought of him as his true father. By the age of seven the father raped him three times. The boy ran away. He survived four days in the desert by eating cactus and scorpions. He was finally found by a drug cartel boss, who took the boy in. Mr. Sanchez was his name, and he was immediately impressed by the boys intelligence and grown up attitude. Mr. Sanchez provided the boy with an education at a private prep school and a nice house. The boy had sex for the first time when he was 12, the next year he graduated high school. He was the school's star running back. The boy was lost, although he liked Mr. Sanchez, he never thought of him as his father. The boy went to India, were he became a spiritual leader of a large tribe of farmers. With his power he turned the several plots that each tribe member had into a large corporate farm and became a millionaire. His assets grew as he bought into other successful start ups. He often got ahead in business with his great leadership abilities, but once in a while he used violence. This was India after all. By the time he was 22, the boy had all the money he would ever need. So he bought a yacht and traveled the world for 10 years. For 10 years he visited every major port, slept with girls from every country imaginable, and tried every drug ever made (by n""")
+
+spam.append("""
+Omg hai _____^ I’m Roberto-san and I absolutely luuuv @@ anime Emoticón heart and my fav is naurto!!! Okies so anyways, im going to tell you about the BEST day of my life when I met my hot husband sasuke!! <333333333 OMFGZ HE WAS SOOOOO FREAKIN KAWAII IN PERSON!!! Supa kawaii desu!!!!!!!! ^___________________­_______^ When I walked onto Tokyo street ==I looked up and saw…SASUKE!!!!!!!!!!­!!!!! <3333333333333333333­33333333333333333333­33333333333333333333­333!!!! “ KONNICHIWA OMGZZZZZZZZZZZZZZZZ SUPA SUPA SUPA KAWAII SASUKE-SAMA!!!!!” I yelled nn then he turned chibi then un-chibi!! he looked at me [O.O;;;;;;;;;;;] and then he saw how hot I am ** he grabbed my hand and winked ~^ then pulled me behind a pocky shop o_o and started to kiss me!!!!!! [OMG!!! HIS TOUNGE TASTED LIKE RAMEN!!! RLY!! >.> <.< Emoticón upset (O) (O) (O)] then I saw some baka fat bitch watching us and I could tell she was undressing him with her eyes!!!!!!! [ -____-;;;;; OMG I COULDN’T BELIEVE IT EITHER!!! (ò_ó) (ò_ó) (ò_ó)] so I yelled “UH UH BAKA NEKO THAT’S MY MAN WHY DON’T YOU GO HOOK UP WITH NARUTO CAUSE SASUKE-SAMA LOVES ME!!! (ò_ó)” then sasuke held me close == and said he would only ever love me and kissed me again!!!!!!! ** (O)/­ then we went to his apartment and banged all night long and made 42 babies and they all became ninjas!!!!!!!!!!!!!!! Nyaaaaa!!! (<) ^___________;;;;;;;;­;;;;;;;;""")
+
+spam.append("""
+bubble gum 😋🍬 bubble gum in 🏻 a dish 🍽🏼 how many 🤔 times 🕐🕟🕓 can i succ that dicc send this to 🔟 of your 🏼sweetest 🍭 hoes""")
+
+spam.append("""
+My girlfriend is cute and smart and she's an anime faggot like me. The other night she and I were cuddling in bed and she started humping me and whispering "oniichan oniichan" and that turned me the fuck on so I called her "oneechan" and then she stopped and looked at me and I said "what is it" to which she replied "I always wanted a twin brother so we could fuck all the time" (she's an only child and all I have is a younger brother) so all night long we were humping and calling each other oniichan and oneechan and I came in my underwear and we were pretending we were brother and sister trying to sexually please each other without having sex and it was fucking hot
+
+Original source is unknown, found on a youtube comment""")
+
+spam.append("""
+While it is nice to fuck mlp plushies, the manes get extremely sticky if you just leave it out. Especially if you're like me and get extremely intimate with the pony.
+
+1 Have you ever seen an episode?
+
+2 I mainly fap to the equestria girls versions. I'm not really into the pony versions.
+
+3 I have my little pony bedsheets
+
+4 I have an ikea display case with mlp merch inside.
+
+5 I have a framed autographed picture of fluttershy from the voice actress.
+
+6 my friends are into it too
+
+Now fuck off and let me enjoy this amazing cartoon.""")
+
+spam.append("""
+💯NicE fUckin ✔✔meme 💯m8y t0o bad 💯the sp0opky pirate is 0n his way tO steal ur mEmeS repOst this 💯to keep💯💯 ur b00ty""")
+
+spam.append("""
+I'm pretty sure my roommate is the mysterious hacker known as 4chan, and I'm pretty fucking scared
+>be me yesterday
+>class ended early so I head home
+>get home early and walk to my room
+>hear roommate in his room talking loudly
+>he must not know I'm home
+>probably just singing to his music or something
+>listen a little longer and realize he isn't singing
+>says some weird computer stuff
+>"I'll make a GUI using Visual Basic to trace their IP"
+>shit sounds foreign to me but I know it can't be good
+>bust into his room and am shocked
+>he has his back turned to me so I can see his computer
+>he's typing really fast in green words on an all black background
+>ohfuckwhatdoesthatm­ean.filename
+>see in big letter at the top of the screen "hack_celebrity_pict­ures.exe"
+>oh no
+>he turns around and looks at me
+>I can only see his eyes and mouth through his ski mask
+>ohshitthisisbad.jay­peg
+>he yells "GET THE FUCK OUT"
+>slam his door and rush into my room
+>lock the door so he can't get to me
+>remember I have a computer in my room
+>he could probably hack that into to get to me
+>throw it out the window
+I'm posting this on my laptop right now. I think I'm safe using this because it has something called AVG which basically makes it hacker proof. I'm really scared right now. He's really good with computers and I'm afraid he might find a way to hack the lock on my door""")
+
+spam.append("""
+Seriously? After 20 minutes of getting to choke this gorgeous girl with his cock, two little poots of cum is all he could manage? Pathetic. I registered an account just so I could leave this comment in hopes he sees it one day. Fronting like you're some alpha male, while Elizabeth is performing her heart out, and what do you give her? Two tiddlywinks of cum? I've never felt so personally offended by porn before. You're a real asshole, pal""")
+
+spam.append("""
+Ｎａｎｉ ｔｈｅ ｆｕｃｋ ｄｉｄ ｙｏｕ ｊｕｓｔ ｆｕｃｋｉｎｇ ｉｉｍａｓｕ ａｂｏｕｔ ｗａｔａｓｈｉ， ｙｏｕ ｃｈｉｉｓａｉ ｂｉｔｃｈ ｄｅｓｕｋａ？ Ｗａｔａｓｈｉ’ｌｌ ｈａｖｅ ａｎａｔａ ｋｎｏｗ ｔｈａｔ ｗａｔａｓｈｉ ｇｒａｄｕａｔｅｄ ｔｏｐ ｏｆ ｍｙ ｃｌａｓｓ ｉｎ Ｎｉｈｏｎｇｏ ３， ａｎｄ ｗａｔａｓｈｉ’ｖｅ ｂｅｅｎ ｉｎｖｏｌｖｅｄ ｉｎ ｉｒｏｉｒｏｎａ Ｎｉｈｏｎｇｏ ｔｕｔｏｒｉｎｇ ｓｅｓｓｉｏｎｓ， ａｎｄ ｗａｔａｓｈｉ ｈａｖｅ ｏｖｅｒ ｓａｎｂｙａｋｕ ｐｅｒｆｅｃｔ ｔｅｓｔ ｓｃｏｒｅｓ． Ｗａｔａｓｈｉ ａｍ ｔｒａｉｎｅｄ ｉｎ ｋａｎｊｉ， ａｎｄ ｗａｔａｓｈｉ ｉｓ ｔｈｅ ｔｏｐ ｌｅｔｔｅｒ ｗｒｉｔｅｒ ｉｎ ａｌｌ ｏｆ ｓｏｕｔｈｅｒｎ Ｃａｌｉｆｏｒｎｉａ． Ａｎａｔａ ａｒｅ ｎｏｔｈｉｎｇ ｔｏ ｗａｔａｓｈｉ ｂｕｔ ｊｕｓｔ ａｎｏｔｈｅｒ ｗｅａｂｏｏ． Ｗａｔａｓｈｉ ｗｉｌｌ ｋｏｒｏｓｕ ａｎａｔａ ｔｈｅ ｆｕｃｋ ｏｕｔ ｗｉｔｈ ｖｏｃａｂｕｌａｒｙ ｔｈｅ ｌｉｋｅｓ ｏｆ ｗｈｉｃｈ ｈａｓ ｎｅｖｅｒ ｂｅｅｎ ｍｉｍａｓｕ’ｄ ｂｅｆｏｒｅ ｏｎ ｔｈｉｓ ｃｏｎｔｉｎｅｎｔ， ｍａｒｋ ｗａｔａｓｈｉｎｏ ｆｕｃｋｉｎｇ ｗｏｒｄｓ． Ａｎａｔａ ｔｈｉｎｋｓ ａｎａｔａ ｃａｎ ｇｅｔ ａｗａｙ ｗｉｔｈ ｈａｎａｓｈｉｍａｓｉｎｇ ｔｈａｔ ｋｕｓｏ ｔｏ ｗａｔａｓｈｉ ｏｖｅｒ ｔｈｅ ｉｎｔａａｎｅｔｏ？ Ｏｍｏｕ ａｇａｉｎ， ｆｕｃｋｅｒ． Ａｓ ｗｅ ｈａｎａｓｈｉｍａｓｕ， ｗａｔａｓｈｉ ａｍ ｃｏｎｔａｃｔｉｎｇ ｗａｔａｓｈｉｎｏ ｓｅｃｒｅｔ ｎｅｔｔｏ ｏｆ ｏｔａｋｕｓ ａｃｒｏｓｓ ｔｈｅ ＵＳＡ， ａｎｄ ａｎａｔａｎｏ ＩＰ ｉｓ ｂｅｉｎｇ ｔｒａｃｅｄ ｒｉｇｈｔ ｎｏｗ ｓｏ ｙｏｕ ｂｅｔｔｅｒ ｊｕｎｂｉｓｈｉｍａｓｕ ｆｏｒ ｔｈｅ ａｍｅ， ｕｊｉｍｕｓｈｉ． Ｔｈｅ ａｍｅ ｔｈａｔ ｋｏｒｏｓｕ’ｓ ｔｈｅ ｐａｔｈｅｔｉｃ ｃｈｉｉｓａｉ ｔｈｉｎｇ ａｎａｔａ ｃａｌｌｓ ａｎａｔａｎｏ ｌｉｆｅ． Ｙｏｕ’ｒｅ ｆｕｃｋｉｎｇ ｓｈｉｎｉｍａｓｈｉｔａ’ｄ， ａｋａｃｈａｎ．""")
+
+spam.append("""
+What's good u candy cane cum guzzler DICKCEMBER is here and u know what that means ❄ HOE HOE HOE season has arrived put on your rudolph pasties pop ur peppermint pussy 🍬 and sit by the fireplace with some hot COCKLATE if u want to get RAWED under the mistletoe 🌿 this christmAss send this to 15 of your sluttiest elves if u get 0 back ur an UGLY GRINCH if u get 5 back ur a SEXY SNOWMAN if u get 10 back ur a BAD JINGLE BELL BITCH SHARE in 69 seconds or you won't be gettin dicked down in 2016""")
+
+spam.append("""
+ГРЯЗНАЯ КОММУНИСТИЧЕСКАЯ СКОТИНА ВАЛИ С МОЕЙ КРОВЬЮ ПОТОМ И ГОВНОМ ЗАСЛУЖЕННОЙ ЗЕМЛИ ДАВАЙ ВАЛИ МЫ НЕ ЗАОДНО С РУБЛЕГОЛОВЫМИ УБЛЮДКАМИ ВРОДЕ ТЕБЯ ЭТО СТРАНА ПЕРВОГО МИРА И ТУТ МЫ НЕ ПИХАЕМ БОШКИ В ЖОПЫ МЕДВЕДЯМ И НЕ ГЛУХЧЕМ КАРТОФЕЛЬНЫЙ СОК ДНИ НАПРОЛЕТ ТЫ МАЛЕНЬКАЯ КАЛАШКА-КАКАШКА СПОРИМ Я ТЕБЯ СДЕЛАЮ В ГОНКЕ ОПС СОРРИ УЖЕ ГОТОВО ДЕЛО НАСА НАСА НА СОСИ ЫХЫХЫ ПОШЕЛ ОБРАТНО К СТАНКУ ХЫХЫ А Я ТУТ УЧИТЬ СЫНА НА ВЕЛИКЕ КАТАТЬСЯ БУДУ И ЕЩЕ У МЕНЯ ДЕЛОВАЯ ВСТРЕЧА С ДЖОНСОНОМ О СМОТРИ-КА А ВОТ И ОН ЗВОНИТ ХЕЛЛОУ ДЖОНСОН ХАУ ИЗ ЗЕ БИЗНЕС О ПРЕКРАСНО ММММ ДАА НЕСОМНЕННО НУ В ОБЩЕМ Я ЗАКОНЧИЛ ПЛАТИТЬ ПО СЧЕТАМ И ПОЙДУ ПОИГРАЮ В БИЛЬЯРД УВИДИМСЯ НА РАБОТЕ ЩЕЛК""")
+
+spam.append("""
+WHAT ARE YOU OF THE SAYINGS, KOMRADE? CAPITALISM IS GOOD LIKE YOUR MOM GOOD IN BED. IS NOT. BEAR IS OF THE STRONGEST ANIMAL IN MOTHERLAND, EAGLE IS BUT PUNY RATBIRD. WE MAY NOT WIN RACE THAT IS SPRINT, BUT WIN MARATHON- GUESS WHO SENDS YOU CAPITALIST PIGS TO INTERNATIONAL ORBITAL STATION? DA. WE USE THE SUPERIOR KALASHNIKOV BECAUSE WE NOT OF USING SILLY PLASTIC MATTEL TOY THAT IS OF DESIGNATION "M4"'.
+
+FATHER LENIN WOULD BE OF THE PROUDEST OF ME FOR FENDING OFF WESTERN SWINE SUCH AS BE YOU""")
+
+spam.append("""
+DIRTY COMMIE SCUM GET OFF MY HARD EARNED LAND I EARNED WITH MY OWN SWEAT TEARS AND FECES GET OUT OF HERE WE DONT SUPPORT RUBLE HEADED BASTARDS LIKE YOU THIS IS A FIRST WORLD COUNTRY WHERE WE DONT SHOVE OUR HEADS INTO BEARS ASS AND DRINK POTATO JUICE ALL DAY U LIL KALASHNIKUNT I BET I CAN REK U IN A RACE OWP SORRY ALREADY HAPPENED COSMONAUT MORE LIKE KOSMO-NOT AMIRIT HYUK HYUKHYUK GO BACK TO YOUR BREAD LINE HAR HAR MEANWHILE ILL BE HERE TEACHING MY SON HOW TO RIDE MY BIKE AND MEET JOHNSON IN THAT PRESS MEETING WE HAVE O LOOK IM GETTING A CALL FROM HIM RIGHT NOW OH HELLO JOHNSON HOW IS THE BUSINESS REPORT GOING SPLENDID MHM YES INDEED WELL IVE FINISHED PAYING MY BILLS AND GOING TO PLAY BILLIARDS SEE YOU AT WORK CLICK"""
+)
+
+spam.append("""
+bomb me the FUCK up🕙🇵🇸🕙🇵🇸🕙 terrorist shit terrorist sHit thats 🏽some terrorist shit right🕙th 🇵🇸 ere🕙🕙 right✔there ✔✔if i do ƽaү so my selｆ i say so🇵🇸. thats what im talking about right thereright there(chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ🇵🇸🏽🇵🇸НO0ОଠＯOOＯOОଠଠOooo­ᵒᵒᵒᵒᵒᵒᵒᵒᵒ🕙🕙🕙🇵🇸🕙🇵🇸terrorist shit""")
+
+spam.append("""
+hallo i am stephun haewkung i can cret wrumhols i am also a scientist bcus i can maek math from a book. lets invunt a tiem mashin e=mc squrd see? i'm also inventiv i inventd a therem about spase and teim also pls tech me how 2 maek this cluck. best regads stephun huwkong genie us of this univrose. Pee Ess: tull mecrosuft to give me things 2.
+
+Hi, My name is Ahmed, I'm new. My hobbies are clock making (like Rolex), making curry stew (like Wolfgang Puck), and going to church (we call it a mosque). I'm really just your everyday, Western citizen. I wish America accepted me for the dankness of the memes I make, not the color of my skin or the he style of clothes I wear. Like Bearnie Sanders always says, "Keep your chin up lil' pimp".
+
+Edit: Thank Sagan I'm getting upboats and not being oppressed!
+
+Edit 2: Praise Allah I mean yay science XD can't wait to see The Martian, that film will certainly enlighten me!""")
+
+spam.append("""
+Just me and my 💕daddy💕, hanging out I got pretty hungry so I started to pout He asked if I was down for something yummy and I asked what and he said he'd give me his cummies! Yeah! Yeah!💕 I drink them! I slurp them! I swallow them whole It makes daddy happy so it's my only goal... 💕Harder daddy! Harder daddy! 💕 1 cummy, 2 cummy, 3 cummy, 4 I'm daddy's princess but I'm also a whore! He makes me feel squishy💗!He makes me feel good💜! 💘💘💘He makes me feel everything a little should!~ 💘💘💘 👑💦💘Wa-What!💘💦👑""")
+
+spam.append("""
+IM DELETING YOU, DADDY! ██]]]]]]]]]]]]]]]]]]­]]]]]]]]]]]]]]]]] 10% complete..... ████]]]]]]]]]]]]]]]]­]]]]]]]]]]] 35% complete.... ███████]]]]]]]]]]]]]­]]] 60% complete.... ███████████] 99% complete..... 🚫ERROR!🚫 💯True💯 Daddies are irreplaceable I could never delete you Daddy! Send this to ten other 👪Daddies👪 who give you cummies Or never get called squishy again😬😬❌❌ If you get 0 Back: no cummies for you 🚫🚫👿 3 back: you're squishy☁️💦 5 back: you're daddy's kitten😽👼💦
+
+Just me and my 💕daddy💕, hanging out I got pretty hungry🍆 so I started to pout 😞 He asked if I was down ⬇for something yummy 😍🍆 and I asked what and he said he'd give me his 💦cummies!💦 Yeah! Yeah!💕💦 I drink them!💦 I slurp them!💦 I swallow them whole💦 😍 It makes 💘daddy💘 😊happy😊 so it's my only goal... 💕💦😫Harder daddy! Harder daddy! 😫💦💕 1 cummy💦, 2 cummy💦💦, 3 cummy💦💦💦, 4💦💦💦💦 I'm 💘daddy's💘 👑princess 👑but I'm also a whore! 💟 He makes me feel squishy💗!He makes me feel good💜! 💘💘💘He makes me feel everything a little should!~ 💘💘💘 👑💦💘Wa-What!💘💦👑""")
+
+spam.append("""
+ IT'S MACKDONALDS TIME🍗🍗 You have been visited by the legendary RONALD MCDONALD, master of BURGER 🍗🍗 Upbeetis this within 3 seconds or you'll never have BURGER without HEARTBURN
+""")
